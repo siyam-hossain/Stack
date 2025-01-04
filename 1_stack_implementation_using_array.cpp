b96dad69c4cd68c02f45e17904a90ec6ad6acecb
Siyam Hossain <siyam.cybersoul@gmail.com>
2025-01-04 22:58:37 +0600

    update

--- 1_stack_implementation_using_array.cpp
+++ 1_stack_implementation_using_array.cpp
@@ -31,6 +31,41 @@ class mystack
 
 int main(int argc, char const *argv[])
 {
+    //creating static object
+    mystack stk;
+
+    stk.push(10);
+    stk.push(20);
+    stk.push(30);
+    stk.push(40);
+    cout<<"after push: ";
+    cout<<stk.top()<<endl;
+
+    stk.pop();
+    cout<<"after pop: ";
+    cout<<stk.top()<<endl;
     
+    cout<<"size: ";
+    cout<<stk.size()<<endl;
+
+    cout<<"is empty: ";
+    cout<<stk.empty()<<endl;
+
+    stk.pop();
+    stk.pop();
+    stk.pop();
+
+    // to avoide run time error while printing top when stack empty
+    //it provide runtime error 
+    // cout<<stk.top()<<endl;
+    
+    //this isn't
+    // if(stk.empty() == false)
+    if(!stk.empty())
+    {
+        cout<<stk.top()<<endl;
+    }
+    //similar case for pop
+
     return 0;
 }
