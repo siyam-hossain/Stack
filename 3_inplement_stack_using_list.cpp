9534e344cd0c6e6095b8b874520259c6dc034908
Siyam Hossain <siyam.cybersoul@gmail.com>
2025-01-04 23:14:01 +0600

    update

---
+++ 3_inplement_stack_using_list.cpp
@@ -0,0 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class mystack
+{
+    public:
+    //here is the STL list
+        list<int> s;
+
+    void push(int val)
+    {
+        s.push_back(val);
+    }
+    void pop()
+    {
+        s.pop_back();
+    }
+    int top()
+    {
+        return s.back();
+    }
+    int size()
+    {
+        return s.size();
+    }
+    bool empty()
+    {
+        return s.empty();
+    }
+    
+};
+
+int main(int argc, char const *argv[])
+{
+    mystack stk;
+
+    int sz;
+    cin>>sz;
+    for (int i = 0; i < sz; i++)
+    {
+        int x;
+        cin>>x;
+        stk.push(x);
+    }
+    //Last in first out LIFO
+    //last->.......->first
+    while (!stk.empty())
+    {
+        cout<<stk.top()<<" ";
+        stk.pop();
+    }cout<<endl;
+    
+    return 0;
+}
